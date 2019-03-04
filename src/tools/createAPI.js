51127bbd7c9e9a5a204d6d12b98b9759f89bca5c
Jacob McSwain <jacob@clevyr.com>
2019-03-04 11:56:30 -0600

    Try again

--- src/tools/createAPI.js
+++ src/tools/createAPI.js
@@ -88,6 +88,7 @@ function copyTemplate(appPath, originalDirectory, settings) {
     return new Promise((resolve, reject) => {
         console.log(originalDirectory);
         const templatePath = originalDirectory + '/src/templates' + settings.TypeScript ? 'typescript' : 'javascript';
+        console.log(templatePath);
         if (fs.existsSync(templatePath)) {
             fs.copy(templatePath, appPath).then(() => {
                 renderEJS(appPath, settings).then(() => {
